e2ebc4a038f047e043bc558c7532c9c7e4eef641
akiogitgit <akimaron2828@icloud.com>
2023-07-11 08:59:56 +0900

    feat: panda css導入

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true, // 追加
+  },
+}
 
 module.exports = nextConfig
